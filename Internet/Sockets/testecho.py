684555220e18f212a7d11ab77191c14f65578006
guillermo <gmozlopez@gmail.com>
2015-06-30 06:29:01 -0700

    Working on testecho.py

--- Internet/Sockets/testecho.py
+++ Internet/Sockets/testecho.py
@@ -1,2 +1,15 @@
 import sys
-from PP4E.launchmodules import QuietPortableLaucd ncher
+
+print(sys.path)
+
+from PP4E.launchmodules import QuietPortableLauncher
+
+numclients = 8
+def start(cmdline):
+	QuietPortableLauncher(cmdline, cmdline)()
+
+#start('echo-server.py')
+
+args = ' '.join(sys.argv[1:])
+for i in range(numclients):
+	start('echo-client.py %s' % args)
