f01e4dab978fecefdab6df6137d04e63d378c758
Guillermo Lopez <glopez@godaddy.com>
2017-01-23 20:37:29 -0700

    added preview dir

---
+++ Preview/make_db_file.py
@@ -0,0 +1,21 @@
+#! python3
+
+"""
+Save in-memory db object to file with custom formatting;
+"""
+
+dbfilename = 'people-file'
+ENDDB = 'enddb.'
+ENDREC = 'endrec.'
+RECSEP = '=>'
+
+def storeDbase(db, dbfilename=dbfilename):
+    "formatted dump of database to flat file"
+    dbfile = open(dbfilename, 'w')
+    for key in db:
+        print(key, file=dbfile)
+        for (name, value ) in db[key].items():
+            print(name + RECSEP + repr(value), file=dbfile)
+        print(ENDREC, file=dbfile)
+    print(ENDDB, file=dbfile)
+    dbfile.close()
